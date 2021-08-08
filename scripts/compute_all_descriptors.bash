24aa56dbfea65e8fb143dca2ead70cc6086dd97d
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2021-08-08 13:04:36 +0300

    Restructure. Recreate tactile_exploration dataset

---
+++ scripts/compute_all_descriptors.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DATASET_DIR=../python/datasets
+LEN_DATASET_DIR=${#DATASET_DIR}
+
+ESF_EXECUTABLE=../build/compute_esf
+SHOT_EXECUTABLE=../build/compute_shot
+#echo $LEN_DATASET_DIR
+
+OUTDIR=../descriptors/
+mkdir $OUTDIR
+find  $DATASET_DIR -print0 | while IFS= read -r -d '' file
+do 
+    extension="${file: -4}"
+    # echo $extension
+    # echo "$file"
+    if [ $extension = ".pcd" ]
+    then
+        echo "PCD to process"
+        OUTFILE="$OUTDIR${file:$LEN_DATASET_DIR +1}" 
+        BASE_FILE=${OUTFILE%????} # descriptors/dataset_type/baseball
+        echo "Base file: $BASE_FILE"
+
+        PCD_IN=$file
+
+        # Compute ESF
+        ESF_OUT=${BASE_FILE}_esf.csv
+        echo  $PCD_IN $ESF_OUT
+        $ESF_EXECUTABLE $PCD_IN $ESF_OUT
+
+        # Compute SHOT
+        # KEYPOINTS_OUT="${BASE_FILE}_shot_keypoints"
+        KEYPOINTS_OUT="_"
+        SHOT_OUT="${BASE_FILE}_shot" # cpp will append .csv automatically
+        echo . $SHOT_EXECUTABLE $PCD_IN $KEYPOINTS_OUT $SHOT_OUT
+        $SHOT_EXECUTABLE $PCD_IN $KEYPOINTS_OUT $SHOT_OUT
+
+    elif [ $extension = ".csv" ]
+    then
+        echo "ignoring csv"
+
+    elif [ $extension = 'E.md' ]
+    then
+        echo "ignoring readme"
+    else
+        echo "mkdir here"
+        FOLDER="$OUTDIR${file:$LEN_DATASET_DIR +1}"
+        echo $FOLDER
+        mkdir $FOLDER
+    fi
+done
+
+
+
---
+++ src/compute_esf.cpp
@@ -0,0 +1,61 @@
+#include <pcl/features/esf.h>
+#include <pcl/io/pcd_io.h>
+
+typedef pcl::PointXYZ PointType;
+typedef pcl::ESFSignature640 DescriptorType;
+
+void saveDescriptors(pcl::PointCloud<DescriptorType>::Ptr model_descriptors, std::string filename);
+
+int main(int argc, char *argv[])
+{   
+    pcl::ESFEstimation<PointType, DescriptorType> ESF;
+    if (argc < 3)
+    {
+        std::cout << "Incorrect number of parameters: \n";
+        std::cout << " - arg1: pcd model path; \n";
+        std::cout << " - arg2: path to save filename; \n";
+        return 0;
+    }
+
+    pcl::PointCloud<PointType>::Ptr model(new pcl::PointCloud<PointType>());
+    pcl::PointCloud<DescriptorType>::Ptr model_descriptors(new pcl::PointCloud<DescriptorType>());
+    pcl::PointCloud<PointType>::Ptr output(new pcl::PointCloud<PointType>());
+
+    //
+    //  Load clouds
+    //
+    if (pcl::io::loadPCDFile(argv[1], *model) < 0)
+    {
+        std::cout << "Error loading model cloud." << std::endl;
+        return (-1);
+    }
+
+    std::string fname = argv[2];
+
+    ESF.setInputCloud(model);
+    ESF.compute(*model_descriptors);
+
+    std::cout << "Number of K neighbours %" << ESF.getKSearch() << std::endl;
+    // std::cout << "Search radius " << ESF.getRadiusSearch() << std::endl;
+    // std::cout << "Search method " << ESF.getSearchMethod() << std::endl;
+    // std::cout << "Search parameter " << ESF.getSearchParameter() << std::endl;
+
+    saveDescriptors(model_descriptors, fname);
+}
+
+void saveDescriptors(pcl::PointCloud<DescriptorType>::Ptr model_descriptors, std::string filename)
+{
+	std::ofstream myfile;
+	myfile.open (filename);
+	for (int i = 0; i < model_descriptors->size(); i++ )
+	{
+		for (int j = 0; j < 640; j++ ) {
+            myfile << model_descriptors->points[i].histogram[j] << ",";
+            if ((j+1)%64 == 0){
+                myfile << "\n";
+            }
+        }
+		myfile << "\n";
+	}
+	myfile.close();
+}
